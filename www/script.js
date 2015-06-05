f20bbb593021e59e1179349170748cc5545a1606
2645 <guesse_who_this_is@hotmail.com>
2015-06-05 16:17:25 +0200

    testing

--- www/script.js
+++ www/script.js
@@ -9,8 +9,27 @@ document.addEventListener("deviceready", onDeviceReady, false);
 // Cordova is ready to be used!
 //
 function onDeviceReady() {
-   console.log(device);
-   console.log(window.device);
-   console.log(window.plugins);
+    console.log(device);
+    console.log(window.device);
+    console.log(window.plugins);
+
+    setInterval(getAccel(), 100);
+    setInterval(getRot(), 100);
+
 }
 
+function getAccel() {
+    navigator.accelerometer.getCurrentAcceleration(onSuccessAccel, onErrorAccel);
+}
+
+function onSuccesAccel(acceleration){
+    $('#accel').html('Acceleration X: ' + acceleration.x + '\n' + 'Acceleration Y: ' + acceleration.y + '\n' + 'Acceleration Z: ' + acceleration.z + '\n' + 'Timestamp: ' +a cceleration.timestamp + '\n');
+}
+
+function onErrorAccel(){
+    $('#accel').html('ERROR');
+}
+
+function getRot() {
+
+}
