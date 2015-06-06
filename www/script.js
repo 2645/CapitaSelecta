f72ae4c361e621f4a70a2289849b301be31acb60
2645 <guesse_who_this_is@hotmail.com>
2015-06-06 14:37:41 +0200

    maxAccel

--- www/script.js
+++ www/script.js
@@ -25,9 +25,14 @@ function onSuccessAccel(acceleration) {
     var xAccel = Math.round(acceleration.x);
     var yAccel = Math.round(acceleration.y);
     var zAccel = Math.round(acceleration.z);
+
+    var totalAccel = Math.pow(Math.pow(Math.abs(xAccel), 3) + Math.pow(Math.abs(yAccel), 3) + Math.pow(Math.abs(zAccel), 3), 1 / 3);
+    if(totalAccel> maxAccel){
+        maxAccel = totalAccel;
+    }
     
     $('#accel').html('Acceleration X: ' + xAccel + '<br>' + 'Acceleration Y: ' + yAccel + '<br>' + 'Acceleration Z: ' + zAccel);
-    
+
     $('#record').html(maxAccel);
 
 };
